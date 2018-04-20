191113f31a1c326845654c5aa0b4aa43c763e414
Kirit Tanna <kirittanna@gmail.com>
2018-04-20 13:32:50 -0400

    wip: cms setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/content/`,
         name: 'markdown-pages',
       },
     },
